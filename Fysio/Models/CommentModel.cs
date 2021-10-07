cb4bd2f805810731218fc1f67bf9fe64ce114225
Bas <bbuijsen@gmail.com>
2021-10-07 15:38:25 +0200

    small changes

--- Fysio/Models/CommentModel.cs
+++ Fysio/Models/CommentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace Fysio.Models
 {
     public class CommentModel
     {
+        [Required(ErrorMessage = "Zichtbaarheid is verplicht")]
         public bool VisibleForPatient { get; set; }
+        [Required(ErrorMessage = "Een opmerking is verplicht")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Er is iets fout gegaan. Probeer het later nog eens.")]
         public int PatientFileId { get; set; }
         public CommentModel()
         {
