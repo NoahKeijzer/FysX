86cdcb975a170f0d4e35b7c27c9c8c47aa536cb8
Bas <bbuijsen@gmail.com>
2021-10-15 15:24:11 +0200

    added tests for br 2

---
+++ DomainTest/BR_4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DomainTest
+{
+    public class BR_4
+    {
+        //Bij een aantal behandelingen is een toelichting verplicht. 
+
+        [Fact]
+        public void test()
+        {
+
+        }
+    }
+}
--- EFInfrastructure/DBAddAppointmentService.cs
+++ EFInfrastructure/DBAddAppointmentService.cs
@@ -24,69 +24,11 @@ namespace EFInfrastructure
 
         public bool AddAppointment(Appointment appointment, PatientFile patientFile)
         {
-            IsPossibleTime(appointment.Treator, appointment.AppointmentDateTime, patientFile.TreatmentPlan.MinutesPerSession);
-            Availability availabilityTreator = availabilityRepository.GetAvailabilityForTreator(appointment.Treator);
-            List<Appointment> appointmentsOnDay = appointmentRepository.GetAppointmentsForDateForTreator(appointment.Treator, appointment.AppointmentDateTime);
-            List<Appointment> appointmentsForPatient = appointmentRepository.GetAppointmentsForDateForPatient(appointment.Patient, appointment.AppointmentDateTime);
-            bool IsAvailable = true;
-            foreach(Appointment a in appointmentsOnDay)
+            if(IsPossibleTime(appointment.Treator, appointment.AppointmentDateTime, patientFile.TreatmentPlan.MinutesPerSession))
             {
-                if(!(a.AppointmentDateTime.TimeOfDay > appointment.EndDateTime.TimeOfDay || a.EndDateTime.TimeOfDay < appointment.AppointmentDateTime.TimeOfDay))
-                {
-                    IsAvailable = false;   
-                }
-            }
-            foreach(Appointment a in appointmentsForPatient)
-            {
-                if(!(a.AppointmentDateTime.TimeOfDay > appointment.EndDateTime.TimeOfDay || a.EndDateTime.TimeOfDay < appointment.AppointmentDateTime.TimeOfDay))
-                {
-                    IsAvailable = false;
-                }
-            }
-            if (IsAvailable)
-            {
-                if (appointment.AppointmentDateTime.DayOfWeek == System.DayOfWeek.Monday)
-                {
-                    if (appointment.AppointmentDateTime.TimeOfDay >= availabilityTreator.MOStartTime.TimeOfDay && appointment.EndDateTime.TimeOfDay <= availabilityTreator.MOEndTime.TimeOfDay)
-                    {
-                        appointmentRepository.AddAppointment(appointment);
-                        return true;
-                    }
-                }
-                else if (appointment.AppointmentDateTime.DayOfWeek == System.DayOfWeek.Tuesday)
-                {
-                    if (appointment.AppointmentDateTime.TimeOfDay >= availabilityTreator.TUStartTime.TimeOfDay && appointment.EndDateTime.TimeOfDay <= availabilityTreator.TUEndTime.TimeOfDay)
-                    {
-                        appointmentRepository.AddAppointment(appointment);
-                        return true;
-                    }
-                }
-                else if (appointment.AppointmentDateTime.DayOfWeek == System.DayOfWeek.Wednesday)
-                {
-                    if (appointment.AppointmentDateTime.TimeOfDay >= availabilityTreator.WEStartTime.TimeOfDay && appointment.EndDateTime.TimeOfDay <= availabilityTreator.WEEndTime.TimeOfDay)
-                    {
-                        appointmentRepository.AddAppointment(appointment);
-                        return true;
-                    }
-                }
-                else if (appointment.AppointmentDateTime.DayOfWeek == System.DayOfWeek.Thursday)
-                {
-                    if (appointment.AppointmentDateTime.TimeOfDay >= availabilityTreator.THStartTime.TimeOfDay && appointment.EndDateTime.TimeOfDay <= availabilityTreator.THEndTime.TimeOfDay)
-                    {
-                        appointmentRepository.AddAppointment(appointment);
-                        return true;
-                    }
-                }
-                else if (appointment.AppointmentDateTime.DayOfWeek == System.DayOfWeek.Friday)
-                {
-                    if (appointment.AppointmentDateTime.TimeOfDay >= availabilityTreator.FRStartTime.TimeOfDay && appointment.EndDateTime.TimeOfDay <= availabilityTreator.FREndTime.TimeOfDay)
-                    {
-                        appointmentRepository.AddAppointment(appointment);
-                        return true;
-                    }
-                }
-            }
-            
+                appointmentRepository.AddAppointment(appointment);
+                return true;
+            }  
             return false;
         }
 
