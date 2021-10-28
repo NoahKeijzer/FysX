5cba64bf819256d8f057bb72778d2f41676a793f
Bas <bbuijsen@gmail.com>
2021-10-28 16:22:33 +0200

    fix

---
+++ Fysio/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fysio.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Login", "Account", new { area = "Treator" });
+        }
+
+        public IActionResult AccesDenied()
+        {
+            return RedirectToAction("Login", "Account", new { area = "Treator" });
+        }
+    }
+}
